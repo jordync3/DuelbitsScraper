9318ff5964345b0d819fc59136eb7fd53ab9cfdb
Jordyn Gamble <twinsgamble@gmail.com>
2022-07-11 15:59:25 -0400

    added args

--- AllBets.js
+++ AllBets.js
@@ -10,7 +10,7 @@ function wsdb(callback){
 
 
   async function startBrowser() {
-    const browser = await puppeteer.launch({headless : true});
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'],headless : true});
     const page = await browser.newPage();
     return {browser, page};
   }
